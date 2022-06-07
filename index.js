4bda3b3b4c2bd240bf978c4f71c14f70aaf7f9c2
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-07 16:52:38 +0300

    completing asignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function razzle(){
+    console.log("You've been razzled!");
+}
+razzle();
+
+function scares(lawyer= "Martin", target= "'sheep"){
+    console.log(`${lawyer} scares ${target}!`)
+}
+scares();
+ 
+function saturdayFun(activity="roller-skate"){
+    return`This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work= "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(result="*"){
+    return function wrapAdjective(behaviour="a special kid"){
+    return `You are ${result}${behaviour}${result}!`;
+}
+}
